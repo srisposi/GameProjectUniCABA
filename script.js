cd4c396894c77c52330eb5245ed23bcf5751949a
srisposi <sdrisposi@gmail.com>
2022-10-11 20:44:30 -0300

    Keyboard Inputs

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ window.addEventListener("load", function () {
   canvas.width = 500;
   canvas.height = 500;
 
-  class InputHandler {}
+  class InputHandler {
+    constructor(game) {
+      this.game = game;
+      window.addEventListener("keydown", (e) => {
+        if (
+          (e.key === "ArrowUp" || e.key === "ArrowDown") &&
+          this.game.keys.indexOf(e.key) === -1
+        ) {
+          this.game.keys.push(e.key);
+        }
+        console.log(this.game.keys);
+      });
+      window.addEventListener("keyup", (e) => {
+        if (this.game.keys.indexOf(e.key) > -1) {
+          this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+        }
+        console.log(this.game.keys);
+      });
+    }
+  }
   class Projectile {}
   class Particle {}
   class Player {
@@ -16,8 +35,12 @@ window.addEventListener("load", function () {
       this.x = 20;
       this.y = 100;
       this.speedY = 0;
+      this.maxSpeed = 3;
     }
     update() {
+      if (this.game.keys.includes("ArrowUp")) this.speedY = -this.maxSpeed;
+      else if (this.game.includes("ArrowDown")) this.speedY = this.maxSpeed;
+      else this.speedY = 0;
       this.y += this.speedY;
     }
     draw(context) {
@@ -33,6 +56,8 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.keys = [];
     }
     update() {
       this.player.update();
