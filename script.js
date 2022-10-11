af9d4956fe3fce8ed79a729a92c7475f03435d7b
srisposi <sdrisposi@gmail.com>
2022-10-11 20:05:19 -0300

    Creating the Player and Game objects

--- script.js
+++ script.js
@@ -8,10 +8,39 @@ window.addEventListener("load", function () {
   class InputHandler {}
   class Projectile {}
   class Particle {}
-  class Player {}
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+    update() {
+      this.y += this.speedY;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
   class Enemy {}
   class Layer {}
   class Background {}
   class UI {}
-  class Game {}
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {
+      this.player.update();
+    }
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
 });
