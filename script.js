9ce26b355e3031fd949fc537847526d70ca8f892
srisposi <sdrisposi@gmail.com>
2023-05-05 21:38:54 -0300

    feature/Lucky Fish Enemy Class

--- script.js
+++ script.js
@@ -172,6 +172,20 @@ window.addEventListener("load", function () {
     }
   }
 
+  class LuckyFish extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 99;
+      this.height = 95;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("lucky");
+      this.frameY = Math.floor(Math.random() * 2);
+      this.lives = 3;
+      this.score = 15;
+      this.type = "lucky";
+    }
+  }
+
   class Layer {
     constructor(game, image, speedModifier) {
       this.game = game;
@@ -334,8 +348,9 @@ window.addEventListener("load", function () {
     }
     addEnemy() {
       const randomize = Math.random();
-      if (randomize < 0.5) this.enemies.push(new Angler1(this));
-      else this.enemies.push(new Angler2(this));
+      if (randomize < 0.3) this.enemies.push(new Angler1(this));
+      else if (randomize < 0.6) this.enemies.push(new Angler2(this));
+      else this.enemies.push(new LuckyFish(this));
       console.log(this.enemies);
     }
     checkCollision(rect1, rect2) {
