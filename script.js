ab584abf44c5b346e5e03074ca81c48f88a9c9eb
srisposi <sdrisposi@gmail.com>
2023-05-06 06:11:12 -0300

    feature/Dust Effect Animation

--- script.js
+++ script.js
@@ -339,6 +339,58 @@ window.addEventListener("load", function () {
       this.layers.forEach((layer) => layer.draw(context));
     }
   }
+
+  class Explosion {
+    constructor(game, x, y) {
+      this.game = game;
+      this.x = x;
+      this.y = y;
+      this.frameX = 0;
+      this.spriteHeight = 200;
+      this.fps = 15;
+      this.timer = 0;
+      this.interval = 1000 / 15;
+      this.markedForDeletion = false;
+      this.maxFrame = 8;
+    }
+    update(deltaTime) {
+      this.x -= this.game.speed;
+      if (this.timer > this.interval) {
+        this.frameX++;
+        this.timer = 0;
+      } else {
+        this.timer += deltaTime;
+      }
+      if (this.frameX > this.maxFrame) this.markedForDeletion = true;
+    }
+    draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  class SmokeExplosion extends Explosion {
+    constructor(game, x, y) {
+      super(game, x, y);
+      this.image = document.getElementById("smokeExplosion");
+      this.spriteWidth = 200;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = x - this.width * 0.5;
+      this.y = y - this.height + 0.5;
+    }
+  }
+
+  class FireExplosion extends Explosion {}
   class UI {
     constructor(game) {
       this.game = game;
@@ -403,6 +455,7 @@ window.addEventListener("load", function () {
       this.keys = [];
       this.enemies = [];
       this.particles = [];
+      this.explosions = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.ammo = 40;
@@ -433,10 +486,15 @@ window.addEventListener("load", function () {
       this.particles = this.particles.filter(
         (particle) => !particle.markedForDeletion
       );
+      this.explosions.forEach((explosion) => explosion.update(deltaTime));
+      this.explosions = this.explosions.filter(
+        (explosion) => !explosion.markedForDeletion
+      );
       this.enemies.forEach((enemy) => {
         enemy.update();
         if (this.checkCollision(this.player, enemy)) {
           enemy.markedForDeletion = true;
+          this.addExplosion(enemy);
           for (let i = 0; i < enemy.score; i++) {
             this.particles.push(
               new Particle(
@@ -471,6 +529,7 @@ window.addEventListener("load", function () {
                 );
               }
               enemy.markedForDeletion = true;
+              this.addExplosion(enemy);
               if (enemy === "hive") {
                 for (let i = 0; i < 5; i++) {
                   this.enemies.push(
@@ -504,6 +563,9 @@ window.addEventListener("load", function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+      this.explosions.forEach((explosion) => {
+        explosion.draw(context);
+      });
       this.background.layer4.draw(context);
     }
     addEnemy() {
@@ -512,6 +574,17 @@ window.addEventListener("load", function () {
       else if (randomize < 0.6) this.enemies.push(new Angler2(this));
       else if (randomize < 0.8) this.enemies.push(new HiveWhale(this));
       else this.enemies.push(new LuckyFish(this));
+    }
+    addExplosion() {
+      const randomize = Math.random();
+      if (randomize < 1)
+        this.explosions.push(
+          new SmokeExplosion(
+            this,
+            enemy.x + enemy.width * 0.5,
+            enemy.y + enemy.height * 0.5
+          )
+        );
     }
     checkCollision(rect1, rect2) {
       return (
@@ -530,8 +603,8 @@ window.addEventListener("load", function () {
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.update(deltaTime);
     game.draw(ctx);
+    game.update(deltaTime);
     requestAnimationFrame(animate);
   }
   animate(0);
