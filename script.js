b073cecc156b367fb4ae88be06ce025de06b87ee
srisposi <sdrisposi@gmail.com>
2023-05-06 05:41:47 -0300

    feature/Drone Enemy Class

--- script.js
+++ script.js
@@ -285,7 +285,21 @@ window.addEventListener("load", function () {
       this.speedX = Math.random() * -1.2 - 0.2;
     }
   }
-
+  class Drone extends Enemy {
+    constructor(game, x, y) {
+      super(game);
+      this.width = 115;
+      this.height = 95;
+      this.x = x;
+      this.y = y;
+      this.image = document.getElementById("drone");
+      this.frameY = Math.floor(Math.random() * 2);
+      this.lives = 3;
+      this.score = this.lives;
+      this.type = "drone";
+      this.speedX = Math.random() * -4.2 - 0.5;
+    }
+  }
   class Layer {
     constructor(game, image, speedModifier) {
       this.game = game;
@@ -423,7 +437,7 @@ window.addEventListener("load", function () {
         enemy.update();
         if (this.checkCollision(this.player, enemy)) {
           enemy.markedForDeletion = true;
-          for (let i = 0; i < 10; i++) {
+          for (let i = 0; i < enemy.score; i++) {
             this.particles.push(
               new Particle(
                 this,
@@ -447,7 +461,7 @@ window.addEventListener("load", function () {
               )
             );
             if (enemy.lives <= 0) {
-              for (let i = 0; i < 5; i++) {
+              for (let i = 0; i < enemy.score; i++) {
                 this.particles.push(
                   new Particle(
                     this,
@@ -457,7 +471,17 @@ window.addEventListener("load", function () {
                 );
               }
               enemy.markedForDeletion = true;
-
+              if (enemy === "hive") {
+                for (let i = 0; i < 5; i++) {
+                  this.enemies.push(
+                    new Drone(
+                      this,
+                      enemy.x + Math.random() * enemy.width,
+                      enemy.y + Math.random() * enemy.height * 0.5
+                    )
+                  );
+                }
+              }
               if (!this.gameOver) this.score += enemy.score;
               if (this.score > this.winningScore) this.gameOver = true;
             }
